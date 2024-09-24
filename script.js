fc0747a0cab9b509e474479583e4b896d29266c1
Laura Souza <Laauramirellydesouza@gmail.com>
2024-09-23 21:21:13 -0300

    Button
    button

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ document.getElementById('open_btn').addEventListener('click', function () {
     sidebar.classList.toggle('open-sidebar');
     main.classList.toggle('shifted');
 });
+
+function addInput() {
+    const container = document.querySelector('.input-group');
+    const newInputBox = document.createElement('div');
+    newInputBox.classList.add('input-box');
+    newInputBox.innerHTML = `
+        <label for="newItem">Novo Item</label>
+        <input type="text" name="newItem" placeholder="Digite o novo item" required>
+    `;
+    // Adiciona o novo input abaixo de todos os outros inputs, exceto a textarea
+    container.appendChild(newInputBox);
+}
